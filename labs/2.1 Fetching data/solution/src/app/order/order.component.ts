beb8d08d729806c12947a5b0c5b56374b64d36d8
rapPayne <bob@creator.net>
2023-08-31 21:09:24 -0500

    Updated solution to latest Angular version

--- labs/2.1 Fetching data/solution/src/app/order/order.component.ts
+++ labs/2.1 Fetching data/solution/src/app/order/order.component.ts
@@ -1,6 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-order',
@@ -9,47 +9,31 @@ import { HttpClient } from '@angular/common/http';
 })
 export class OrderComponent {
   order!: any;
-  constructor(
-    private _activatedRoute: ActivatedRoute,
-    private _http: HttpClient //  <-- Add this
-  ) { }
-  ngOnInit() {
+  constructor(private _activatedRoute: ActivatedRoute, private _http: HttpClient) { }
+  ngOnInit(): void {
     const orderId = this._activatedRoute.snapshot.params['orderId'];
     this._http.get(`http://localhost:3008/api/orders/${orderId}`).subscribe(
       {
         next: (data) => { this.order = data; }
       });
-    // this.order = {
-    //   id: orderId,
-    //   orderTime: "2099-03-30T18:26:13.161Z",
-    //   pickupTime: "2099-03-30T18:38:03.286Z",
-    //   area: "Theater 1",
-    //   location: "Table 6",
-    //   tax: 3.89,
-    //   tip: 9.44,
-    //   items: [                               // <-- Add this
-    //     { id: 1, itemId: 9, price: 11.75, }, // <-- Add this
-    //     { id: 2, itemId: 1, price: 9.54, },   // <-- Add this
-    //     { id: 3, itemId: 1, price: 8.34, },  // <-- Add this
-    //     { id: 4, itemId: 8, price: 10.25, }, // <-- Add this
-    //   ],                                     // <-- Add this
-    //   status: "completed"
-    // }
+    // this.order = { id: orderId, }; // <-- Remove your hardcoded order
   }
 
-  // getSubtotal(order: any) {
-  //   let subTotal = 0
-  //   for (let item of order.items) {
-  //     subTotal += item.price;
+  getSubtotal(order: any): number {
+    return order?.items?.reduce((acc: number, item: any) =>
+      acc + item.price, 0);
+  }
+  // getSubtotal(order: any): number {
+  //   if (!order?.items) return 0;
+  //   let total = 0;
+  //   for (let item of order?.items) {
+  //     total += item.price;
   //   }
-  //   return subTotal;
+  //   return total;
   // }
 
-  getSubtotal(order: any) {
-    return order?.items?.reduce((acc: number, item: any) => acc + item.price, 0);
-  }
-
-  getTotal(order: any) {
+  getTotal(order: any): number {
     return this.getSubtotal(order) + order?.tax + order?.tip;
   }
+
 }
