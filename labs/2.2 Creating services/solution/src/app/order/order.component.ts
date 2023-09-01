2caab61a7af2fa9d9e9103512dcd69fd12007728
rapPayne <bob@creator.net>
2023-08-31 21:10:24 -0500

    Updated solution to latest Angular version

--- labs/2.2 Creating services/solution/src/app/order/order.component.ts
+++ labs/2.2 Creating services/solution/src/app/order/order.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OrdersService } from '../orders.service';
@@ -9,24 +10,29 @@ import { OrdersService } from '../orders.service';
 })
 export class OrderComponent {
   order!: any;
-
-  constructor(
-    private _ordersService: OrdersService,  //  <-- Add this
-    private _activatedRoute: ActivatedRoute,
-  ) { }
-
-  ngOnInit() {
+  constructor(private _activatedRoute: ActivatedRoute, private _ordersService: OrdersService) { }
+  ngOnInit(): void {
     const orderId = this._activatedRoute.snapshot.params['orderId'];
     this._ordersService.getOrder(orderId).subscribe({
       next: (data) => { this.order = data; }
     });
   }
 
-  getSubtotal(order: any) {
-    return order?.items?.reduce((acc: number, item: any) => acc + item.price, 0);
+  getSubtotal(order: any): number {
+    return order?.items?.reduce((acc: number, item: any) =>
+      acc + item.price, 0);
   }
+  // getSubtotal(order: any): number {
+  //   if (!order?.items) return 0;
+  //   let total = 0;
+  //   for (let item of order?.items) {
+  //     total += item.price;
+  //   }
+  //   return total;
+  // }
 
-  getTotal(order: any) {
+  getTotal(order: any): number {
     return this.getSubtotal(order) + order?.tax + order?.tip;
   }
+
 }
